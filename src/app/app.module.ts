d6fd67e84fbe0fcd57a4d1498ca4fb96154db920
Milica Pajic <pajicmilica.31@gmail.com>
2021-02-14 20:07:49 +0100

    zavrsena todo-item komponenta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
