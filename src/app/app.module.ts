f8cba9f6b3b70ce1e5623a9c3b9461760e566d4b
Milica Pajic <pajicmilica.31@gmail.com>
2021-02-14 21:12:51 +0100

    dodate sve tri komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { DoneItemComponent } from './done-item/done-item.component';
+import { UrgentItemComponent } from './urgent-item/urgent-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    DoneItemComponent,
+    UrgentItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/urgent-item/urgent-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-urgent-item',
+  templateUrl: './urgent-item.component.html',
+  styleUrls: ['./urgent-item.component.css']
+})
+export class UrgentItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
