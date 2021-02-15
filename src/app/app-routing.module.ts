58455253ab2fdf2967fa8b2c61ed057ac4e0c5e0
Milica Pajic <pajicmilica.31@gmail.com>
2021-02-15 21:03:52 +0100

    dodaj linkove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from './homepage/homepage.component';
+import {TodoAddComponent} from "./todo-add/todo-add.component";
+import {UrgentAddComponent} from "./urgent-add/urgent-add.component";
 
 const routes: Routes = [
  {path: '', component: HomepageComponent},
+ {path: 'add/todo', component: TodoAddComponent},
+ {path: 'add/urgent', component: UrgentAddComponent}
 ];
 
 @NgModule({
