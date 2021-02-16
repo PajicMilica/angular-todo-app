1619c914c93891c47c0be73925f46a9f4a68211d
Milica Pajic <pajicmilica.31@gmail.com>
2021-02-16 16:07:23 +0100

    todo urgent dodavanje u storage

--- src/app/urgent-add/urgent-add.component.ts
+++ src/app/urgent-add/urgent-add.component.ts
@@ -5,11 +5,61 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './urgent-add.component.html',
   styleUrls: ['./urgent-add.component.css']
 })
+
 export class UrgentAddComponent implements OnInit {
 
-  constructor() { }
+  static TODO_URGENT_KEY = 'urgent-todo-list';
+  urgentInput: string;
+  finalDate: string;
+  showMessageError: boolean;
+  minDate: string;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.showMessageError = false;
+    this.minDate = this.getMinDate();
+  }
+
+  saveTodoUrgentItem(event) {
+    if (!this.urgentInput || this.urgentInput === '' || !this.finalDate || this.finalDate === '') {
+      this.showMessageError = true;
+      event.preventDefault();
+      return;
+    }
+
+
+    //ubaci u skladiste
+    var todoUrgentListArrayString = localStorage.getItem(UrgentAddComponent.TODO_URGENT_KEY);
+
+    if(todoUrgentListArrayString) {
+      let urgent = {
+        item: this.urgentInput,
+        date: this.finalDate
+      }
+      let todoUrgentListArray = JSON.parse(todoUrgentListArrayString);
+      todoUrgentListArray.push(urgent);
+      console.log(todoUrgentListArray);
+      localStorage.setItem(UrgentAddComponent.TODO_URGENT_KEY, JSON.stringify(todoUrgentListArray));
+    }else{
+      let urgent = {
+        item: this.urgentInput,
+        date: this.finalDate
+      }
+      const urgentToDo= [urgent];
+      console.log(urgentToDo);
+      localStorage.setItem(UrgentAddComponent.TODO_URGENT_KEY,JSON.stringify(urgentToDo));
+    }
+    alert("Todo urgent item sucessfully added: " + this.urgentInput + " until: " + this.finalDate);
+    this.urgentInput = "";
+    this.finalDate = "";
+  }
+
+  getMinDate(): string {
+    const date = new Date();
+    const dateString = date.toISOString().slice(0, 10);
+    console.log(dateString);
+    return dateString;
+  }
 }
